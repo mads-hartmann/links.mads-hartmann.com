88282abe58cb26d86f825e937be2022632780c83
Mads Hartmann <mads379@gmail.com>
2021-07-22 20:02:30 +0000

    Add a bit of debugging

--- ci/create-toc.sh
+++ ci/create-toc.sh
@@ -3,9 +3,12 @@
 set -euo pipefail
 
 # Make sure we're in the root of the repository.
-cd "$(git rev-parse --show-toplevel)"
+path="$(git rev-parse --show-toplevel)"
+echo "Changing directory to ${path}"
+cd "${path}"
 
 # Format all tracked markdown files.
+echo "Running markdoc-toc on files"
 git ls-tree -r main --name-only \
 | grep -E '.md$' \
 | xargs -I{} ./node_modules/.bin/markdown-toc -i '{}'
