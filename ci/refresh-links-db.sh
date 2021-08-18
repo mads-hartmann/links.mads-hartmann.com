0b0238fc66d23220f3bdd6d34d8c411881edcc45
Mads Hartmann <mads379@gmail.com>
2021-08-18 17:48:26 +0000

    Improve refresh-links-db and run it

--- ci/refresh-links-db.sh
+++ ci/refresh-links-db.sh
@@ -2,5 +2,15 @@
 
 set -euo pipefail
 
-airtable-export data "${AIRTABLE_BASE_ID}" Links --sqlite nextjs/public/data/links.db
-sqlite-utils enable-fts nextjs/data/links.db Links Title airtable_id
+PATH_TO_NEXTJS_DB="nextjs/public/data/links.db"
+
+if [[ -f "${PATH_TO_NEXTJS_DB}" ]]; then
+    echo "Deleting existing database at ${PATH_TO_NEXTJS_DB}"
+    rm "${PATH_TO_NEXTJS_DB}"
+fi
+
+echo "Generating new SQLite DB at ${PATH_TO_NEXTJS_DB}"
+airtable-export data "${AIRTABLE_BASE_ID}" Links --sqlite "${PATH_TO_NEXTJS_DB}"
+
+echo "Enabling FTS"
+sqlite-utils enable-fts "${PATH_TO_NEXTJS_DB}" Links Title airtable_id
