7b2e09601805793bdc127e271d3c285b066a71ab
Mads Hartmann <mads379@gmail.com>
2021-08-28 20:06:51 +0000

    Add topics endpoint

---
+++ nextjs/lib/download-db.ts
@@ -0,0 +1,27 @@
+import fs from 'fs'
+
+import fetch from 'node-fetch';
+import { promisify } from 'util';
+const writeFilePromise = promisify(fs.writeFile);
+
+function downloadFile(url: string, outputPath: string) {
+    return fetch(url)
+        .then(x => x.arrayBuffer())
+        .then(x => writeFilePromise(outputPath, Buffer.from(x)));
+}
+
+export async function download(uri: string, filename: string) {
+
+    if (fs.existsSync(filename)) {
+        console.log('File already exists.')
+        return
+    }
+
+    try {
+        console.log('Downloading file')
+        await downloadFile(uri, '/tmp/links.db');
+        console.log('All done');
+    } catch (error) {
+        console.log('Download failed', error)
+    }
+};
